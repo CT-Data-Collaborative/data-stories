ca9ebbd52ab5d04cadffdaee55392dbf80468c0c
Sasha Cuerda <sasha@mapconomy.com>
2015-03-26 02:49:02 +0100

    fixed stupid mapping problems

--- aging/static/js/map2.js
+++ aging/static/js/map2.js
@@ -1,42 +1,40 @@
 function map() {
-  // Time slider
-  // $("#slider").show();
-  var geojson, info;
+  var geojson, info, stateLayer;
   var layerObj = {};
-  var yearKey = {"percent15": 2015, "percent20":2020, "percent25": 2025};
   var layerKey = {2015: "percent15", 2020: "percent20", 2025:"percent25"}
-  var yearVars = ["percent15", "percent20", "percent25"];
   var mapYears = [2015,2020,2025];
   var currentLayer = mapYears[0];
 
-  function addSlider(yearArray, groupOfLayers, map) {
-    var slid = false;
+  function addSlider(yearArray) {
     firstYear = yearArray[0];
     lastYear = yearArray[yearArray.length - 1];
     $("#firstyear").text(firstYear);
     $("#lastyear").text(lastYear);
     $("#mapslider").slider();
-
     $('#mapslider').on('change', function(slideEvt) {
-        map.removeLayer(groupOfLayers[currentLayer]);
         year = slideEvt.value;
-        // console.log(year);
         currentLayer = year.newValue;
-        map.addLayer(groupOfLayers[currentLayer]);
+        setVariable(year.newValue);
     });
   }
 
-  function getColor(d) {
-      return d > 0.2    ? '#F2E2E5' :
-             d > 0.17   ? '#C1D0E3' :
-             d > 0.15   ? '#7E9FC5' :
-             d > 0.13   ? '#0C527F' :
-                          '#0A0F1C';
-  }
+  // function getColor(d) {
+  //     return d > 0.2    ? '#F2E2E5' :
+  //            d > 0.17   ? '#C1D0E3' :
+  //            d > 0.15   ? '#7E9FC5' :
+  //            d > 0.13   ? '#0C527F' :
+  //                         '#0A0F1C';
+  // }
 
-  function mapColor(feature) {
-    var k = layerKey[currentLayer]
-    var perOv65 = feature.properties[k];
+  function getColor(d) {
+      return d > 0.2    ? '#0A0F1C' :
+             d > 0.17   ? '#0C527F' :
+             d > 0.15   ? '#7E9FC5' :
+             d > 0.13   ? '#C1D0E3' :
+                          '#F2E2E5';
+  }
+  function mapColor(layer, variableName) {
+    var perOv65 = layer.feature.properties[variableName];
     if (typeof perOv65 == 'undefined') {
         return "#787878";
     } else {
@@ -45,34 +43,33 @@ function map() {
   }
 
   function style(feature) {
-    console.log(currentLayer);
-      return {
-          weight: 2,
-          opacity: 1,
-          color: 'white',
-          dashArray: '3',
-          fillOpacity: 0.9,
-          fillColor: mapColor(feature)
+    return {
+        weight: 2,
+        opacity: 1,
+        color: 'white',
+        dashArray: '3',
+        fillOpacity: 0.9,
+        fillColor: mapColor(feature)
       };
-  }
+    }
 
-    info = L.control();
+  info = L.control();
 
-    info.onAdd = function (map) {
-        this._div = L.DomUtil.create('div', 'info');
-        this.update();
-        return this._div;
-    };
+  info.onAdd = function (map) {
+      this._div = L.DomUtil.create('div', 'info');
+      this.update();
+      return this._div;
+  };
 
-    info.update = function (props) {
-        this._div.innerHTML = '<h4>CT Population Over 65</h4>' +  (props ?
-        '<b>' + props.name + '</b><br />' + props.percentOver65*100 + ' %'
-                : 'Hover over a town');
-    };
+  info.update = function (props) {
+      key = layerKey[currentLayer];
+      this._div.innerHTML = '<h4>CT Population Over 65</h4>' +  (props ?
+      '<b>' + props.name + '</b><br />' + props[key]*100 + ' %'
+              : 'Hover over a town');
+  };
 
   function highlightFeature(e) {
       var layer = e.target;
-      // console.log(layer);
       layer.setStyle({
           weight: 5,
           color: '#666',
@@ -88,8 +85,14 @@ function map() {
   }
 
   function resetHighlight(e) {
-    console.log(e);
-    layerObj[currentLayer].resetStyle(e.target);
+    var layer = e.target;
+    layer.setStyle({
+      weight: 2,
+      opacity: 1,
+      color: 'white',
+      dashArray: '3',
+      fillOpacity: 0.9
+    });
     info.update();
   }
 
@@ -103,38 +106,24 @@ function map() {
   $.getJSON('static/data/towns.json', function(data) {
 
     var boundaries = data;
-
     var map = L.map('map').setView([41.562265, -72.690697], 9);
-
     var baselayer = L.tileLayer("http://server.arcgisonline.com/ArcGIS/rest/services/Canvas/World_Light_Gray_Base/MapServer/tile/{z}/{y}/{x}", {
         attribution: "Tiles &copy; Esri &mdash; Esri, DeLorme, NAVTEQ",
         subdomains: 'abcd',
         minZoom: 0,
         maxZoom: 20
     }).addTo(map);
-
     map.attributionControl.addAttribution('Population data &copy; <a href="http://census.gov/">US Census Bureau</a>');
 
-
-
-
-
-
-    yearVars.forEach(function(year) {
-      boundaries.features.forEach(function(town) {
-        var percent = town.properties[year];
-        town.properties.percentOver65 = percent;
+    // set variable for first layer
+    boundaries.features.forEach(function(town) {
+        town.properties.percentOver65 = town.properties.percent15;;
       });
-      var newLayer = L.geoJson(boundaries, {
-        style: style,
-        onEachFeature: onEachFeature
-      });
-      var y = yearKey[year];
-      layerObj[y] = newLayer;
-    });
 
-    // console.log(layerObj);
-    layerObj[currentLayer].addTo(map);
+    stateLayer = L.geoJson(boundaries, {
+      onEachFeature: onEachFeature
+    }).addTo(map);
+
     info.addTo(map);
     var legend = L.control({position: 'bottomright'});
 
@@ -142,18 +131,14 @@ function map() {
 
         var div = L.DomUtil.create('div', 'info legend'),
         // Since the getColor function is expecting numbers, not strings, I modified this array
-                grades = [0.2,0.17,0.15,0.14,0.13]
+                grades = [0, 0.13, 0.14, 0.15, 0.17, 0.2]
                 labels = [];
 
         for (var i = 0; i < grades.length; i++) {
             var from = grades[i];
             var to = grades[i + 1];
-            // In order to have the percentages, you can just append it on to the string
-            // Also, you can call getColor(from+1) b/c that would be asking for the color value of 1.x with x being the array
-            // val from grades (e.g. 1.13). So instead might need to rework this to pass grades[i+1], grade[i+2] directly in order to get the
-            // right values for the colors. Also, the way this is currently set up, the colors are getting inverted.
             labels.push(
-                    '<i style="background:' + getColor(from) + '"></i> ' +
+                    '<i style="background:' + getColor(from+0.01) + '"></i> ' +
                     Math.round(from*100) + "%" + (Math.round(to*100) ? '&ndash;' + Math.round(to*100) + "%" : '+'));
         }
 
@@ -162,7 +147,25 @@ function map() {
     };
 
     legend.addTo(map);
-    addSlider(mapYears,layerObj,map);
+    addSlider(mapYears);
+    setVariable(mapYears[0]);
   })
 
+// This is not setting a JavaScript
+// variable, but rather the variable by which the map is colored.
+// The input is a string 'currentYear', which specifies which property
+// of the  geoJSON file is used to color the map.
+  function setVariable(currentYear) {
+    variableName = layerKey[currentYear];
+    stateLayer.eachLayer(function(layer) {
+      layer.setStyle({
+        weight: 2,
+        opacity: 1,
+        color: 'white',
+        dashArray: '3',
+        fillOpacity: 0.9,
+        fillColor: mapColor(layer, variableName)
+      })
+    })
+  }
 }
